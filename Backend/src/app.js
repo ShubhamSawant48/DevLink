a0d2768a1a00e144cbc84b596e3ed632a47655c9
shubham <sawantshubham736@gmail.com>
2025-08-06 00:45:27 +0530

    diving into API's

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -12,21 +12,63 @@ connectDB()
     });
   })
   .catch((err) => {
-    console.log("error occurede while connecting db...");
+    console.log("error occured while connecting db...");
   });
 
+app.use(express.json());
+
+// feed api - get all users from db
+app.get("/users", async (req, res) => {
+  try {
+    const users = await User.find({});
+
+    res.send(users);
+  } catch (err) {
+    res.status(500).send("can't find users");
+  }
+});
+
+// signup api
 app.post("/signup", async (req, res) => {
-  const user = new User({
-    firstName: "virat",
-    lastName: "kholi",
-    emailId: "virat@kholi.com",
-    password: "virat123",
-  });
+  const user = new User(req.body);
 
   try {
     await user.save();
     res.send("user added successfully...");
   } catch (err) {
     res.status(500).send("user unable to add");
+  }
+});
+
+// get user by email
+app.get("/user", async (req, res) => {
+  try {
+    const user = await User.find({ emailId: req.body.emailId });
+
+    res.send(user);
+  } catch (err) {
+    res.status(500).send("can't find users");
+  }
+});
+
+// delete user from db
+app.delete("/user", async (req, res) => {
+  try {
+    await User.findByIdAndDelete(req.body.userId);
+
+    res.send("user deleted successfully!");
+  } catch (err) {
+    res.status(500).send("deleting didn't happen!");
+  }
+});
+
+// update user from db
+app.patch("/user", async (req, res) => {
+  try {
+    const user = await User.findByIdAndUpdate(req.body.userId,req.body,{returnDocument:"before",});
+    console.log(user)
+    res.send("user updated successfully!");
+  } catch (err) {
+    res.status(500).send("updating didn't happen!");
   }
 });
--- Backend/src/config/database.js
+++ Backend/src/config/database.js
@@ -1,7 +1,8 @@
+// require('dotenv').config({path: './.env' });
 const mongoose = require('mongoose');
 
 const connectDB = async () => {
-    await mongoose.connect("mongodb+srv://sawantshubham736:8O1VpbdXzsVAXEGw@namastenodejs.pp4h1jb.mongodb.net/devLink")
+    await mongoose.connect("mongodb+srv://sawantshubham736:8O1VpbdXzsVAXEGw@namastenodejs.pp4h1jb.mongodb.net/devLink");
 }
 
 module.exports = connectDB;
