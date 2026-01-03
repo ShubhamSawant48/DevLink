17c411cd285cbce983fe0726b308e1f3e0620913
shubham <sawantshubham736@gmail.com>
2026-01-03 15:59:13 +0530

    component name error

--- Frontend/src/components/Body.jsx
+++ Frontend/src/components/Body.jsx
@@ -1,6 +1,6 @@
 import axios from "axios";
 import Footer from "./Footer";
-import NavBar from "./Navbar";
+import NavBar from "./NavBar";
 import { Outlet, useNavigate } from "react-router-dom";
 import { BASE_URL } from "../utils/constants";
 import { useDispatch, useSelector } from "react-redux";
